6f655abf4af44c38307b6dbcf3061d425845627f
rahulnishad96 <rahulnishadhandia@gmail.com>
2021-08-25 10:29:01 +0530

    add seached comment

--- src/App.js
+++ src/App.js
@@ -1,20 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-import {Switch,Route} from "react-router-dom";
-import HomePage from './pages/homePage/HomePage';
-import DetalsBlog from './pages/detailsPage/DetalsBlog';
-import AddBlog from './pages/addPage/AddBlog';
-import EditBlog from './pages/editPage/EditBlog';
+import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import HomePage from "./pages/homePage/HomePage";
+import DetalsBlog from "./pages/detailsPage/DetalsBlog";
+import AddBlog from "./pages/addPage/AddBlog";
+import EditBlog from "./pages/editPage/EditBlog";
 
 function App() {
   return (
     <div className="App">
       <h1 className="app-title">Blog application with Fetch API</h1>
       <Switch>
-        <Route exact path="/" component={HomePage}/>
-        <Route exact path="/addblog" component={AddBlog}/>
-        <Route exact path ="/detailsblog/:id" component={DetalsBlog}/>
-        <Route exact path="/editblog/:id" component={EditBlog}/>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/addblog" component={AddBlog} />
+        <Route exact path="/detailsblog/:id" component={DetalsBlog} />
+        <Route exact path="/editblog/:id" component={EditBlog} />
       </Switch>
     </div>
   );
--- src/pages/homePage/HomePage.js
+++ src/pages/homePage/HomePage.js
@@ -1,53 +1,74 @@
-import React, { useEffect, useState } from 'react';
-import './HomePage.css';
-import { useHistory } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import "./HomePage.css";
+import { useHistory } from "react-router-dom";
 
-const url= "http://localhost:1000/blog";
-
+const url = "http://localhost:1000/blog";
 
 function HomePage() {
-    const [blogs,setBlogs]=useState([]);
-    const [searchValue, setSearchValue] = useState("");
-    let history = useHistory();
-    
-    useEffect(() => {
-        fetch(url)
-        .then(res => res.json())
-        .then (data =>  setBlogs(data));
-    },[]);
-        
+  const [blogs, setBlogs] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [showMatch,setShowMatching] = useState(false);
+  let history = useHistory();
 
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
 
-    const SearchHandler = (e) =>{
-        e.preventDefault();
-        blogs.map((blog) => {
-          if(blog.title === searchValue)
-          {
-            history.push(`/detailsblog/${blog.id}`); 
-          }
-        });
-    };
-        const list = blogs.map( (blog) => {
-            return (<div key={blog.id} className="blog-outer-div">
-                        <h2>{blog.title}</h2>
-                        <button onClick = {() => history.push(`/detailsblog/${blog.id}`)}>Details</button>
-                    </div>)
-        });
-
-        console.log(list);
+  const SearchHandler = (e) => {
+    e.preventDefault();
+    blogs.map((blog) => {
+      if (blog.title === searchValue) {
+        history.push(`/detailsblog/${blog.id}`);
+      }
+    });
+    setShowMatching(true);
+    setSearchValue("");
+  };
+  const list = blogs.map((blog) => {
     return (
-        <div>
-            <h1>Home Page</h1>
-            <div className="blog-inner-div">
-                <form>
-                    <input placeholder="Search By Title" value ={searchValue}  onChange={(e) => {setSearchValue(e.target.value)}} required/>
-                    <button type="submit" onClick={SearchHandler}>Search</button>
-                </form>
-                <button className="creat-button" type="submit" onClick={() => {history.push("/addblog")}}>Create New Blog</button>
-            </div>
-            {list}
-        </div>
-    )
+      <div key={blog.id} className="blog-outer-div">
+        <h2>{blog.title}</h2>
+        <button onClick={() => history.push(`/detailsblog/${blog.id}`)}>
+          Details
+        </button>
+      </div>
+    );
+  });
+
+  console.log(list);
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <div className="blog-inner-div">
+        <form>
+          <input
+            placeholder="Search By Title"
+            value={searchValue}
+            onChange={(e) => {
+              setSearchValue(e.target.value);
+            }}
+            required
+          />
+          <button type="submit" onClick={SearchHandler}>
+            Search
+          </button>
+        </form>
+        <button
+          className="creat-button"
+          type="submit"
+          onClick={() => {
+            history.push("/addblog");
+          }}
+        >
+          Create New Blog
+        </button>
+      </div>
+      {showMatch && <h2 className="match-text">Match not found</h2>}
+      {list}
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
